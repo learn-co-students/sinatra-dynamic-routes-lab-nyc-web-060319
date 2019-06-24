044bb10b1709a97427e4b203376e41614e527f92
Alexander Riccio <test35965+learn@gmail.com>
2019-06-24 11:36:58 -0400

    solve lab

--- app.rb
+++ app.rb
@@ -1,6 +1,49 @@
 require_relative 'config/environment'
 
+def op(operation, num1, num2)
+  if operation == "add"
+    return num1 + num2
+  elsif operation == "subtract"
+    return num1 - num2
+  elsif operation == "multiply"
+    return num1 * num2
+  elsif operation == 'divide'
+    return num1 / num2
+  end
+end
+
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
 
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase * @number}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    "#{op(@operation, @num1, @num2)}"
+  end
 end
