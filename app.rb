824a157b5b808e4eac78844abd34422de84e2f45
eschechter <schechter.evan@gmail.com>
2019-06-24 12:59:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,47 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
-  # Write your code here!
+  get "/reversename/:name" do
+    @name = params[:name]
+    @name.reverse
+  end
 
+  get "/square/:number" do
+    @num = params[:number]
+    (@num.to_i * @num.to_i).to_s
+  end
+
+  get "/say/:number/:phrase" do
+    @num = params[:number]
+    @phrase = params[:phrase]
+    str = ""
+    @num.to_i.times do
+      str += @phrase
+    end
+    str
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    word1 = params[:word1]
+    word2 = params[:word2]
+    word3 = params[:word3]
+    word4 = params[:word4]
+    word5 = params[:word5]
+    "#{word1} #{word2} #{word3} #{word4} #{word5}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    operator_string = params[:operation]
+    num1 = params[:number1]
+    num2 = params[:number2]
+    if operator_string == "add"
+      (num1.to_i + num2.to_i).to_s
+    elsif operator_string == "subtract"
+      (num1.to_i - num2.to_i).to_s
+    elsif operator_string == "multiply"
+      (num1.to_i * num2.to_i).to_s
+    else
+      (num1.to_i / num2.to_i).to_s
+    end
+  end
 end
